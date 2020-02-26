8d149422c0bd460a2ae4459ef97b82e45797ca68
yanal <yanalotaibi@gmail.com>
2020-02-26 11:08:08 +0200

    Updates

---
+++ precache-manifest.44a097e8f0ae4b7dbd8564c421883a59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994c475ce268614fe6784e4547442508",
+    "url": "/dev_yanal/index.html"
+  },
+  {
+    "revision": "57c55d34cc09625d1124",
+    "url": "/dev_yanal/static/css/main.16efeed4.chunk.css"
+  },
+  {
+    "revision": "e08d9915734995197f80",
+    "url": "/dev_yanal/static/js/2.c2cdada5.chunk.js"
+  },
+  {
+    "revision": "57c55d34cc09625d1124",
+    "url": "/dev_yanal/static/js/main.be1f51c8.chunk.js"
+  },
+  {
+    "revision": "6fdebf453a5b304f2cba",
+    "url": "/dev_yanal/static/js/runtime-main.2b583ef2.js"
+  }
+]);
